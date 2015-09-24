f175521c8008f7aa7fc7255f49b0ab717864a99f
inselaffe <gitalias@outlook.com>
2015-09-24 12:41:53 +0100

    Changes to 'cachematrix.R' for PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+##	Calculate and cache the inverse of a matrix on the first occasion
+##	that the inverse is requested
+##	On subsequent occasions of the inverse being requested, return the 
+##	cached value
 
-## Write a short comment describing this function
-
+##	If inverse of matrix supplied as an argument does not exist, it is 
+##	calculated and stored (as 's')
+##	Returns the inverse as calculated or retrieved from cache	
 makeCacheMatrix <- function(x = matrix()) {
-
+	s <- NULL
+	set <- function(y)  {
+		x <<- y
+		s <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inv) s <<- inv
+	getinv <- function() s
+	list(set = set, get = get,
+		 setinv = setinv,
+		 getinv = getinv
+	)
 }
 
-
-## Write a short comment describing this function
-
+##	Attempts to retrieve cached value of inverse of a matrix that has been 
+##	stored using 'makeCacheMatrix'
+##	If cached value is not available, uses 'solve' to calculate the inverse 
+##	of the matrix
+##	Stores the calculated inverse in 'makeCacheMatrix'
+##	Returns the inverse
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	s <- x$getinv()
+	if (!is.null(s))  {
+		message("getting cached data")
+		return(s)
+	}
+	data <- x$get()
+	s <- solve(data, ...)
+	x$setinv(s)
+	s
 }
